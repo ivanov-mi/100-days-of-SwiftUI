269fa3e95fe8cb8afec34f82955501418afe459f
ivanov-mi <ivanov-mi@outlook.com>
2024-04-15 18:09:59 +0300

    Project3 - Challenge1

---
+++ 04-Project3/01-Challenge1/WeSplit-Project3-Update/WeSplit-Project3-Update/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Martin Ivanov on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
