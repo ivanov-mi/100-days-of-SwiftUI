03f66186220ddab5d64222c079317b38c4ffaf89
ivanov-mi <ivanov-mi@outlook.com>
2024-05-20 19:16:31 +0300

    Project14 - Demo

---
+++ 19-Project14/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Martin Ivanov on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
