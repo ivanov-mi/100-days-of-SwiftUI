ff4d617acedeb4b726d7ee554bb1987b33ca4497
ivanov-mi <ivanov-mi@outlook.com>
2024-05-22 17:26:14 +0300

    Project15 - Demo2: Fixing WordScramble

---
+++ 20-Project15/FixingWordScramble/WordScramble-Project15-Update/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Martin Ivanov on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
