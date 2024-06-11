158ee2203ec3d727e19756b36bd1974c007c303c
ivanov-mi <ivanov-mi@outlook.com>
2024-06-11 18:17:30 +0300

    Project18 - Demo

---
+++ 24-Project18/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Martin Ivanov on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
